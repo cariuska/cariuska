575bea87d7e0fcfce44e063fac0636bad6b561d5
Thiago Cariuska <cariuska@globo.com>
2020-10-07 16:57:52 -0300

    Alteração

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 # app.py - a minimal flask api using flask_restful
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource#, Api
+from flask_cors import CORS
 
 app = Flask(__name__)
-api = Api(app)
+#api = Api(app)
+cors = CORS(app)
 
+'''
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world Novo'}  
+        return {'hello': 'world'}  
 
 api.add_resource(HelloWorld, '/')
+'''
+
+@app.route('/', methods=['GET'])
+def home():
+    return {'hello': 'world Teste'}  
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=80)
---
+++ run.bat
@@ -0,0 +1,9 @@
+set versao=%1
+
+docker build --tag cariuska:%versao% .
+
+docker stop Cariuska
+
+docker rm Cariuska
+
+docker run --name Cariuska -d -p 80:80 cariuska:%versao%
