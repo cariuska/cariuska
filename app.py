32a0af8e0927b10107b1989a93959fe317640650
Thiago Cariuska <cariuska@globo.com>
2020-10-07 14:29:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}  
+        return {'hello': 'world Teste'}  
 
 api.add_resource(HelloWorld, '/')
 
